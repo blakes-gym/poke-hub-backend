649bb1161f90e775f1d3287b260db3e7ecb8dd84
Blake Mason <blakelmason@gmail.com>
2020-01-14 17:02:11 -0800

    all pokemon

--- graphql/index.js
+++ graphql/index.js
@@ -1,40 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server-express')
-
-const { Pokemon } = require('../db/models')
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-    pokemon(id: ID!): Pokemon
-  }
-  type Pokemon {
-    id: ID!
-    name: String
-    sprite: String
-    type1: String
-    type2: String
-    hp: Int
-    attack: Int
-    defense: Int
-    spatk: Int
-    spdef: Int
-    speed: Int
-    wishList: Boolean
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => {
-      console.log('heyo')
-      return 'Hello world!'
-    },
-    pokemon: (parent, { id }) => {
-      console.log('hi')
-      return Pokemon.findOne({ where: { id }, raw: true })
-    }
-  }
-}
+const { ApolloServer } = require('apollo-server-express')
+const typeDefs = require('./types')
+const resolvers = require('./resolvers')
 
 const graphql = new ApolloServer({ typeDefs, resolvers })
 
