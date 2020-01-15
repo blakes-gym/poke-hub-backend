505be16ad248104d0a00314d503b3e34bc9f7bb2
Blake Mason <blakelmason@gmail.com>
2020-01-14 19:19:37 -0800

    graphiql production

--- graphql/index.js
+++ graphql/index.js
@@ -2,6 +2,11 @@ const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./types')
 const resolvers = require('./resolvers')
 
-const graphql = new ApolloServer({ typeDefs, resolvers })
+const graphql = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+})
 
 module.exports = graphql
