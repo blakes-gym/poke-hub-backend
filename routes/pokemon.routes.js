fdd1df176a661b5f1c670a562698a68aa9cf04b1
Kat <katherine.wang429@gmail.com>
2020-01-17 14:09:06 -0800

    Wishlist test route

--- routes/pokemon.routes.js
+++ routes/pokemon.routes.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const { Pokemon } = require('../db/models/Pokemon')
-const { Team } = require('../db/models/Pokemon')
-const { db } = require('../db/index')
+const express = require('express');
+const router = express.Router();
+const { Pokemon } = require('../db/models/Pokemon');
+const { Team } = require('../db/models/Pokemon');
+const { db } = require('../db/index');
 
 //get all pokemon
 
@@ -10,10 +10,9 @@ router.get('/', (req, res) => {
   Pokemon.findAll({
     order: ['id']
   })
-  .then(data => res.send(data))
-  .catch(err => res.status(500).send(err))
-})
-
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send(err));
+});
 
 //Edit a pokemon (moves 1-4, nature, item)
 
@@ -26,19 +25,23 @@ router.put('/', (req, res) => {
       newValue = req.body[key];
     }
   }
-  db.query(`UPDATE pokemons SET "${attribute}"='${newValue}' WHERE id=${pokemonId};`)
-  .then(data => res.send(data))
-  .catch(err => res.status(500).send(err))
-})
+  db.query(
+    `UPDATE pokemons SET "${attribute}"='${newValue}' WHERE id=${pokemonId};`
+  )
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send(err));
+});
 
 //get pokemon from a team
 
 router.get('/team/:id', (req, res) => {
-    let teamId = req.params.id;
-    db.query(`SELECT * FROM pokemons INNER JOIN teams ON ("pokemons"."id"="teams"."pokeId") WHERE "teams"."teamId"=${teamId};`)
+  let teamId = req.params.id;
+  db.query(
+    `SELECT * FROM pokemons INNER JOIN teams ON ("pokemons"."id"="teams"."pokeId") WHERE "teams"."teamId"=${teamId};`
+  )
     .then(data => res.send(data[0]))
-    .catch(err => res.status(500).send(err))
-})
+    .catch(err => res.status(500).send(err));
+});
 
 //toggle pokemon's wishlist status
 
@@ -46,33 +49,56 @@ router.put('/wishlist', (req, res) => {
   let pokemonId = req.body.pokemonId;
   let newValue = req.body.boolean;
   db.query(`UPDATE pokemons SET "wishList"=${newValue} WHERE id=${pokemonId};`)
-  .then(data => res.send(data))
-  .catch(err => res.status(500).send(err))
-})
+    .then(data => res.send(data))
+    .catch(err => res.status(500).send(err));
+});
 
 //retrieve all pokemon from the wishlist
 
 router.get('/wishlist', (req, res) => {
   db.query(`SELECT * from pokemons where "wishList"=true;`)
-  .then(data => res.send(data[0]))
-  .catch(err => res.status(500).send(err))
-})
+    .then(data => res.send(data[0]))
+    .catch(err => res.status(500).send(err));
+});
 
 //seed a simulated a pokemon team
 
 router.get('/teamSeedTest', (req, res) => {
   let miniSeed = [];
   for (let i = 0; i < 7; i++) {
-    miniSeed.push( {
+    miniSeed.push({
       teamId: 1,
-      pokeId: (i + 1),
-      name: "best"
-    })
+      pokeId: i + 1,
+      name: 'best'
+    });
   }
   Team.bulkCreate(miniSeed)
     .then(data => res.send(data))
-    .catch(err => res.status(500).send(err))
-})
+    .catch(err => res.status(500).send(err));
+});
 
+// test wishlist
 
-module.exports = router
+router.get('/wishlistSeedTest', (req, res) => {
+  let promiseArr = [];
+  for (let i = 0; i < 25; i++) {
+    promiseArr.push(
+      new Promise((resolve, reject) => {
+        let randomPokemon = Math.floor(Math.random() * Math.floor(800) + 1);
+        resolve(
+          db.query(
+            `UPDATE pokemons SET “wishList”=true WHERE id=${randomPokemon};`
+          )
+        );
+      })
+    );
+  }
+  Promise.all(promiseArr)
+    .then(data => {
+      res.status(200);
+      res.send('OK');
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+module.exports = router;
