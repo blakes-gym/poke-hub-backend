13b83f0111a86e9fb33ff7fa586a36d53cc33142
Josh Fukagawa <fukagawa.joshua@gmail.com>
2020-01-16 19:07:38 -0800

    Add PUT route for wish list

--- routes/pokemon.routes.js
+++ routes/pokemon.routes.js
@@ -4,11 +4,33 @@ const { Pokemon } = require('../db/models/Pokemon')
 const { Team } = require('../db/models/Pokemon')
 const { db } = require('../db/index')
 
+//get all pokemon
+
 router.get('/', (req, res) => {
   Pokemon.findAll({})
-    .then(data => res.send(data))
+  .then(data => res.send(data))
+  .catch(err => res.status(500).send(err))
+})
+
+//get pokemon from a team
+
+router.get('/team/:id', (req, res) => {
+    let teamId = req.params.id;
+    db.query(`SELECT * FROM pokemons INNER JOIN teams ON ("pokemons"."id"="teams"."pokeId") WHERE "teams"."teamId"=${teamId};`)
+    .then(data => res.send(data[0]))
     .catch(err => res.status(500).send(err))
 })
+
+//add a pokemon to the wishlist
+
+router.put('/wishlist', (req, res) => {
+  let pokemonId = req.body.pokemonId;
+  db.query(`UPDATE pokemons SET "wishList"=true WHERE id=${pokemonId};`)
+  .then(data => res.send(data))
+  .catch(err => res.status(500).send(err))
+})
+
+//seed a simulated a pokemon team
 
 router.get('/teamSeedTest', (req, res) => {
   let miniSeed = [];
@@ -24,11 +46,5 @@ router.get('/teamSeedTest', (req, res) => {
     .catch(err => res.status(500).send(err))
 })
 
-router.get('/team/:id', (req, res) => {
-    let teamId = req.params.id;
-    db.query(`SELECT * FROM pokemons INNER JOIN teams ON ("pokemons"."id"="teams"."pokeId") WHERE "teams"."teamId"=${teamId};`)
-    .then(data => res.send(data[0]))
-    .catch(err => res.status(500).send(err))
-})
 
 module.exports = router
