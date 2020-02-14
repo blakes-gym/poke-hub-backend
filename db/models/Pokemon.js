44e73a39521f676384942918f1f99854565daa7c
Blake Mason <blakelmason@gmail.com>
2020-02-13 21:57:29 -0800

    removed comments

--- db/models/Pokemon.js
+++ db/models/Pokemon.js
@@ -24,34 +24,3 @@ schema.pre('save', function() {
 const Pokemon = mongoose.model('Pokemon', schema)
 
 module.exports = Pokemon
-
-// const Model = require('./Model')
-// const { String, Number, SERIAL, COMPUTED } = require('../constants/types')
-
-// class Pokemon extends Model {
-//   constructor() {
-//     super()
-//     this.columns = {
-//       id: SERIAL,
-//       name: String,
-//       image: String,
-//       icon: String,
-//       type1: String,
-//       type2: String,
-//       hp: Number,
-//       atk: Number,
-//       def: Number,
-//       spAtk: Number,
-//       spDef: Number,
-//       speed: Number,
-//       total: COMPUTED(
-//         'integer generated always as (hp + atk + def + spAtk + spDef + speed) stored'
-//       )
-//     }
-//     this.primaryKey = 'id'
-//   }
-// }
-
-// const pokemon = new Pokemon()
-
-// module.exports = pokemon
--- db/models/Team.js
+++ db/models/Team.js
@@ -15,35 +15,3 @@ const schema = new mongoose.Schema({
 const Team = mongoose.model('Team', schema)
 
 module.exports = Team
-
-// const Model = require('./Model')
-// const { [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}], SERIAL, STRING } = require('../constants/types')
-
-// class Team extends Model {
-//   constructor() {
-//     super()
-//     this.columns = {
-//       id: SERIAL,
-//       name: STRING,
-//       p1: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}],
-//       p2: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}],
-//       p3: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}],
-//       p4: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}],
-//       p5: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}],
-//       p6: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pokemon'}]
-//     }
-
-//     this.foreignKeys = {
-//       p1: 'pokemon',
-//       p2: 'pokemon',
-//       p3: 'pokemon',
-//       p4: 'pokemon',
-//       p5: 'pokemon',
-//       p6: 'pokemon'
-//     }
-//   }
-// }
-
-// const team = new Team()
-
-// module.exports = team
